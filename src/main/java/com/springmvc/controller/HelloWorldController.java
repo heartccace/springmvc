48b5dbfb74e2b3b20f2502381f7f0515159761d1
heartccace <heartccace@gmail.com>
2020-10-21 21:54:26 +0800

    “add

--- src/main/java/com/springmvc/controller/HelloWorldController.java
+++ src/main/java/com/springmvc/controller/HelloWorldController.java
@@ -1,18 +1,39 @@
 package com.springmvc.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.concurrent.*;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @author liushuang
  * @create 2019-08-22 13:52
  */
-@Controller
-public class HelloWorldController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "index";
+public class HelloWorldController {
+    private static ConcurrentHashMap<String,String> map = new ConcurrentHashMap<>();
+    private static ReentrantLock lock = new ReentrantLock();
+
+
+    public static void main(String[] args) {
+        map.put("key", "value");
+        new Thread(() -> {
+            if(map.get("key") != null) {
+                try {
+                    TimeUnit.SECONDS.sleep(2);
+                    System.out.println(map.get("key"));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        new Thread(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+                map.clear();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }).start();
+        // lock.lockInterruptibly();
     }
 }
